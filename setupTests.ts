2b3392522df923d19ecd827c7302251f42fedb3d
krutiy <7545245@ukr.net>
2025-11-09 03:26:03 +0200

    feat: add sidebar component that uses compound component pattern

--- setupTests.ts
+++ setupTests.ts
@@ -1,4 +1,4 @@
-import { expect } from "vitest";
+import { expect, vi } from "vitest";
 import * as matchers from "@testing-library/jest-dom/matchers";
 import { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
 declare module "vitest" {
@@ -8,3 +8,22 @@ declare module "vitest" {
       TestingLibraryMatchers<T, void> {}
 }
 expect.extend(matchers);
+
+vi.mock("react-dom/test-utils", async () => {
+  const actual = await vi.importActual<
+    typeof import("react-dom/test-utils")
+  >("react-dom/test-utils");
+
+  return {
+    ...actual,
+    act: (callback: () => unknown | Promise<unknown>) => {
+      const result = callback();
+      if (result && typeof (result as Promise<unknown>).then === "function") {
+        return (result as Promise<unknown>).then(() => undefined);
+      }
+      return undefined;
+    },
+  };
+});
+
+(globalThis as { IS_REACT_ACT_ENVIRONMENT?: boolean }).IS_REACT_ACT_ENVIRONMENT = true;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,5 @@
-export * from './button'
+export * from './button';
+export * from './card';
+export * from './input';
+export * from './switch';
+export * from './sidebar';
